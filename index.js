b5cbd4bd2c0117d9360aa8006775aa3ee105cbce
alexander <alexandernyaga1999@gmail.com>
2023-09-26 11:31:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  } else {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(NYC){
   // Write your code here!
+  if (NYC === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
+}
+  
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else {
+    return "Thank you.";
+  }else {
+    return "Bye.";
+  
 }
